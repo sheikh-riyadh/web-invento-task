bcb34f79d9d3782c5c333e382dd60b982e4dd389
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-04-01 02:41:35 +0600

    created post reducer and action type

---
+++ src/redux/actionTypes/actionTypes.js
@@ -0,0 +1,8 @@
+export const actionTypes = {
+    GET_POST: "GET_POST",
+    DELETE_POST: "DELETE_POST",
+    UPDATE_POST: "UPDATE_POST",
+    UPDATE_MODAL: "UPDATE_MODAL",
+    LOADING: "LOADING",
+
+}
---
+++ src/redux/reducers/postReducer.js
@@ -0,0 +1,40 @@
+import { actionTypes } from "../actionTypes/actionTypes"
+
+const initialState = {
+    posts: [],
+    updatePost: [],
+    isModalOpen: false,
+    isLoading: false
+}
+
+export const postReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.GET_POST:
+            return {
+                ...state,
+                posts: action.payload,
+            }
+        case actionTypes.DELETE_POST:
+            return {
+                ...state,
+                posts: action.payload
+            }
+        case actionTypes.UPDATE_POST:
+            return {
+                ...state,
+                updatePost: action.payload
+            }
+        case actionTypes.UPDATE_MODAL:
+            return {
+                ...state,
+                isModalOpen: action.payload
+            }
+        case actionTypes.LOADING:
+            return {
+                ...state,
+                isLoading: action.payload
+            }
+        default:
+            return state;
+    }
+}
